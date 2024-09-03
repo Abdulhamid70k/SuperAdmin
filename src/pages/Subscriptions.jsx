7c3dccdbf99f98aa0173f1cc944664f7ed346fc1
jayandra06 <vijethasoftwaresols@gmail.com>
2024-09-03 19:50:35 +0530

    loihjl

--- src/pages/Subscriptions.jsx
+++ src/pages/Subscriptions.jsx
@@ -52,7 +52,7 @@ const Subscriptions = () => {
                 </td>
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                   <p className="text-black dark:text-white">
-                    {packageItem.invoiceDate}
+                    {packageItem.subscriptionHolder}
                   </p>
                 </td>
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
